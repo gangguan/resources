403bde2b174b47755c0864f508d799a02434fe86
gangguan <1187483819@qq.com>
2017-10-11 00:29:48 +0800

    delete no used files

--- gpio_i2c/.gpioi2c.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/usr/src/Hi3521A/Hi3521A_SDK_V1.0.3.0/mpp/extdrv/gpio_i2c/gpioi2c.o := arm-hisiv300-linux-ld -EL    -r -o /usr/src/Hi3521A/Hi3521A_SDK_V1.0.3.0/mpp/extdrv/gpio_i2c/gpioi2c.o /usr/src/Hi3521A/Hi3521A_SDK_V1.0.3.0/mpp/extdrv/gpio_i2c/gpio_i2c.o 
--- gpio_i2c/.tmp_versions/gpioi2c.mod
+++
@@ -1,2 +0,0 @@
-/usr/src/Hi3521A/Hi3521A_SDK_V1.0.3.0/mpp/extdrv/gpio_i2c/gpioi2c.ko
-/usr/src/Hi3521A/Hi3521A_SDK_V1.0.3.0/mpp/extdrv/gpio_i2c/gpio_i2c.o
